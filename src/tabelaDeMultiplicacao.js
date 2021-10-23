d40e11eac4e4878cf1b7c42156f03bb4343cc6d1
julio cesar barbosa da silva <juliocesarb.silva11@gmail.com>
2021-10-22 23:12:13 -0300

    Entrega 45 - TabelaMultiplicação Finalizada

--- src/tabelaDeMultiplicacao.js
+++ src/tabelaDeMultiplicacao.js
@@ -1,9 +1,16 @@
-function nomeDaFuncao() { 
+function tabelaMutliplicacaoArrays(n) { 
     // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
-    // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
-    let x = [];
-    for (let i = 0; i <= 10; i++) {
-        x[i] = [];
-    }
-    console.table(x)
+    // TODO: escrever dois loops aninhados para desenhar o array bidimensional e mostrar o restante da tabela no console.
+    
+    let array = [];
+    
+    for (let linha = 0; linha <= n; linha++) {
+      array[linha] = [];
+      for (let coluna = 0; coluna<=n; coluna++) {
+          array[linha][coluna] = linha * coluna;
+      }
+     }
+        console.table(array);
 }
+
+//console.log(tabelaMutliplicacaoArrays(12));
